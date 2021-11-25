9c0325c500e21afb5830d52d82f05ea4e2f7144a
TuyenQuang <luongtuyenquang77@gmail.com>
2021-11-25 21:07:12 +0700

    Responsive - Header - Open, Close Navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from 'react-redux'
 import logo from '../assets/images/Logo-2.png'
 
 function Header({carts}){
+
+    const headerMobile = useCallback(() => {
+        const app = document.querySelector('.App')
+        const iconOpen = document.querySelector('.header__menu__icon-open')
+        const iconClose = document.querySelector('.header__menu__icon-close')
+        const navList = document.querySelector('.nav')
+        const header = document.querySelector('.header')
+        
+        iconOpen.addEventListener('click', () => {
+            navList.classList.add('nav-active')
+            header.classList.add('playing')
+        })
+
+        iconClose.addEventListener('click', () => {
+            header.classList.remove('playing')
+            navList.classList.remove('nav-active')
+        })
+        app.addEventListener('click', () => {
+            header.classList.remove('playing')
+            navList.classList.remove('nav-active')
+        })
+
+        function handleStopPropagation(e) {
+            e.stopPropagation()
+        }
+
+        iconOpen.addEventListener('click', handleStopPropagation)
+        header.addEventListener('click', handleStopPropagation)
+
+    }, [])
+
+    useEffect(() => {
+        headerMobile()
+    },[headerMobile])
 
     useEffect(() => {
         const header = document.querySelector('.header')
@@ -16,30 +50,28 @@ function Header({carts}){
         <header>
             <div className='header'>
                 <div className='header__container'>
-                    <div className='overlay'>
-                        <ul className='nav'>
-                            <li className='nav__item'>
-                                <NavLink activeClassName="nav__active" exact to='/'>
-                                    <i className='bx bx-home nav__icon'></i>
-                                    Trang chủ
-                                </NavLink>
-                            </li>
-                            <li className='nav__item'>
-                                <NavLink activeClassName="nav__active" to='/danh-muc'>
-                                    <i className='bx bx-category nav__icon'></i>
-                                    Sản phẩm
-                                </NavLink>
-                            </li>
-                            {/* <li className='nav__item'>
-                                <NavLink activeClassName="nav__active" to='/phu-kien'>Phụ kiện</NavLink>
-                            </li>
-                            <li className='nav__item'>
-                                <NavLink activeClassName="nav__active" to='/lien-he'>Liên hệ</NavLink>
-                            </li> */}
-                            <i className="fas fa-times header__menu__icon-close"></i>
-                        </ul>
-                        <i className="fas fa-bars header__menu__icon-open"></i>
-                    </div>
+                    <i className="fas fa-bars header__menu__icon-open"></i>
+                    <ul className='nav'>
+                        <li className='nav__item'>
+                            <NavLink activeClassName="nav__active" exact to='/'>
+                                <i className='bx bx-home nav__icon'></i>
+                                Trang chủ
+                            </NavLink>
+                        </li>
+                        <li className='nav__item'>
+                            <NavLink activeClassName="nav__active" to='/danh-muc'>
+                                <i className='bx bx-category nav__icon'></i>
+                                Sản phẩm
+                            </NavLink>
+                        </li>
+                        {/* <li className='nav__item'>
+                            <NavLink activeClassName="nav__active" to='/phu-kien'>Phụ kiện</NavLink>
+                        </li>
+                        <li className='nav__item'>
+                            <NavLink activeClassName="nav__active" to='/lien-he'>Liên hệ</NavLink>
+                        </li> */}
+                    </ul>
+                    <i className="fas fa-times header__menu__icon-close"></i>
                     <div className='header__logo'>
                         <NavLink to='/'>
                             <img src={logo} alt=''/>
